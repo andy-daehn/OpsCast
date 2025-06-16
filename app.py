ede4b61a59ea7145587318332a8aab1b20b07226
Andy Daehn <andydaehn@gmail.com>
2025-06-16 16:59:15 -0600

    Update Streamlit app

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ if brief:
     st.write(brief['summary'])
 
     # Layout: left = incidents/audio/news, right = map + stats
-    left_col, right_col = st.columns([4, 8])
+    left_col, right_col = st.columns([8, 4])
 
     with right_col:
 
